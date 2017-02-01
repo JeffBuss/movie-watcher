326808b0603625f1ceb4fb7598b06077f80c288e
mlimberg <limbergmike@gmail.com>
2017-02-01 13:11:05 -0700

    history fix attempt

--- app/index.js
+++ app/index.js
@@ -4,13 +4,16 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
+// import { syncHistoryWithStore } from 'react-router-redux';
 
 import App from './containers/appContainer/AppContainer';
 import MovieContainer from './containers/movieContainer/MovieContainer';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
+//syncWithStore
 const store = createStore(rootReducer, devTools);
+// const history = syncHistoryWithStore(browserHistory, store);
 
 const router = (
   <Provider store={store}>
