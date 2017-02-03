5cee0f9f410d7d0416fffd3c2e17af3cecd56bc4
mlimberg <limbergmike@gmail.com>
2017-02-03 13:03:20 -0700

    pulling in favorites based on search term

--- app/containers/searchContainer/SearchContainer.jsx
+++ app/containers/searchContainer/SearchContainer.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { loadMovies } from '../../actions';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadResults: (movies) => {
+      console.log(movies)
+      dispatch(loadMovies(movies))
+    }
+  }
+}
+
+
+
+export default connect(mapDispatchToProps);
