3c4541bcf6a61648bd22c9eefff6bcd2546b6c18
mlimberg <limbergmike@gmail.com>
2017-02-05 22:18:42 -0700

    adjusted api to pull in most recent movies

--- app/components/app/App.jsx
+++ app/components/app/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    fetch('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=74e395a1a0373d4f389e8f007c86c5e7')
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=74e395a1a0373d4f389e8f007c86c5e7&language=en-US')
       .then(data => data.json())
       .then(data => this.props.fetchMovies(data.results))
       .then(() => {
@@ -22,7 +22,7 @@ class App extends Component {
           .then(favs => this.props.getFavorites(favs.data))
         }
       })
-    
+
   }
 
   render() {
