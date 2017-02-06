f5577adc547745a2d2071be127e94af47433b8cd
mlimberg <limbergmike@gmail.com>
2017-02-05 22:47:11 -0700

    add clear button to search that resets latest

--- app/components/search/Search.jsx
+++ app/components/search/Search.jsx
@@ -15,17 +15,35 @@ class Search extends Component {
       .then(data => this.props.loadResults(data.results))
   }
 
+  getLatest() {
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=74e395a1a0373d4f389e8f007c86c5e7&language=en-US')
+      .then(data => data.json())
+      .then(data => this.props.loadResults(data.results))
+      .then(() => this.setState({ searchValue: '' }))    
+  }
+
+  enterKey(e) {
+    console.log('fml');
+  }
+
   render() {
     const { searchValue } = this.state
     return (
       <div className='search-field'>
         <input placeholder='Search Movies'
                className='search-input input'
+               value={this.state.searchValue}
+               onKeyPress={this.enterKey.bind(this)}
                onChange={(e) => this.setState({ searchValue: e.target.value })}/>
         <button className='search-btn btn'
                 disabled={!this.state.searchValue}
                 onClick={() => this.getResults()}>
           SEARCH
+        </button>
+        <button className='search-btn btn'
+                disabled={!this.state.searchValue}
+                onClick={() => this.getLatest()}>
+          CLEAR
         </button>
       </div>
     )
